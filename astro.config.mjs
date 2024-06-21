4b5d5feadd7650b60ddd2ba3956b0b61ce1740ba
Avik-creator <avikm744@gmail.com>
2024-06-21 17:48:46 +0530

    Added Changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
+import cloudflare from "@astrojs/cloudflare";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  output: "server",
+  adapter: cloudflare(),
 });
